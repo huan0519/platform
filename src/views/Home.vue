7c0e8cf4757cad1a498607074fdc4f4e5c63da0a
maxia12 <2633588685@qq.com>
2024-10-10 11:32:34 +0800

    Create Home.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <Vue_header></Vue_header>
+    <div>
+      <Vue_main></Vue_main>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Vue_header from '@/components/vue_header.vue';
+import Vue_main from '@/components/vue_main.vue';
+import ElementUI from 'element-ui';
+import Vue from 'vue';
+
+Vue.component(ElementUI)
+
+export default {
+
+  components: {
+    Vue_header,
+    Vue_main
+  }
+}
+</script>
+
+<style>
+#app{
+  display: flex;
+  flex-direction: column;
+  height: 100vh; /* 视口高度 */
+}
+
+
+
+</style>
