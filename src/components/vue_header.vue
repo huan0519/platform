cd88a768b8e5898c7cff523e2710aa064bbe4b43
maxia12 <2633588685@qq.com>
2025-04-18 18:08:14 +0800

    2
    3

--- src/components/vue_header.vue
+++ src/components/vue_header.vue
@@ -1,6 +1,12 @@
 <template>
 <!--  <div class="scanline-header">-->
   <div>
+      <div class="loading" style="float: left;margin-left: 1rem;padding: 1.25rem">
+        <svg width="64px" height="48px">
+          <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="back"></polyline>
+          <polyline points="0.157 23.954, 14 23.954, 21.843 48, 43 0, 50 24, 64 24" id="front"></polyline>
+        </svg>
+      </div>
     <el-menu :default-active="activeIndex" class="h-el-menu" mode="horizontal">
       <p class="header_line"></p>
       <el-menu-item class="h-el-menu-item" index="homepage" @click="navigate('homepage')">
@@ -13,8 +19,8 @@
         <div class="h-item-group2">icloud</div>
       </el-menu-item>
       <p class="header_line"></p>
-      <el-menu-item style="width: 12rem" class="h-el-menu-item" index="ai_analysis" @click="navigate('ai_analysis')">
-        <div class="h-item-group1">大模型分析</div>
+      <el-menu-item class="h-el-menu-item" index="ai_analysis" @click="navigate('ai_analysis')">
+        <div class="h-item-group1">大模型</div>
         <div class="h-item-group2">AI</div>
       </el-menu-item>
       <p class="header_line"></p>
@@ -176,4 +182,34 @@ export default {
   height: 1rem;
   margin-top: -10px;
 }
+.loading svg polyline {
+  fill: none;
+  stroke-width: 3;
+  stroke-linecap: round;
+  stroke-linejoin: round;
+}
+
+.loading svg polyline#back {
+  fill: none;
+  stroke: #ff4d5033;
+}
+
+.loading svg polyline#front {
+  fill: none;
+  stroke: #ff4d4f;
+  stroke-dasharray: 48, 144;
+  stroke-dashoffset: 192;
+  animation: dash_682 1.4s linear infinite;
+}
+
+@keyframes dash_682 {
+  72.5% {
+    opacity: 0;
+  }
+
+  to {
+    stroke-dashoffset: 0;
+  }
+}
+
 </style>
